12a1390aa08ce6dbca2aefa9cfd840e28598e2f3
dapperAuteur <dapperAuteur@users.noreply.github.com>
2019-11-29 13:39:39 -0700

    add about.js file to imports and template preview

--- src/js/cms.js
+++ src/js/cms.js
@@ -4,6 +4,7 @@ import CMS from "netlify-cms-app";
 // Import main site styles as a string to inject into the CMS preview pane
 import styles from "!to-string-loader!css-loader!postcss-loader!sass-loader!../css/main.css";
 
+import AboutPreview from "./cms-preview-templates/about";
 import HomePreview from "./cms-preview-templates/home";
 import PostPreview from "./cms-preview-templates/post";
 import ProductsPreview from "./cms-preview-templates/products";
@@ -11,6 +12,7 @@ import ValuesPreview from "./cms-preview-templates/values";
 import ContactPreview from "./cms-preview-templates/contact";
 
 CMS.registerPreviewStyle(styles, { raw: true });
+CMS.registerPreviewTemplate("about", AboutPreview);
 CMS.registerPreviewTemplate("home", HomePreview);
 CMS.registerPreviewTemplate("post", PostPreview);
 CMS.registerPreviewTemplate("products", ProductsPreview);
